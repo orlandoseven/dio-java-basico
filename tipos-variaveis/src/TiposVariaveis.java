a76ba04f226a3d31699ee8d841bceef956116687
Ronaldo Nascimento <lp.ronnaldo@gmail.com>
2025-02-03 18:28:05 -0300

    constante

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        int numero = 5;
+        numero = 10;
+        //constante
+        final String BR = "Brasil";
+
+        System.out.println(numero);
+    }
+}
